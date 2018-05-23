1972ec90871eb68e0eef53ffe834fc34a4b98dfe
NGCHUNKEUNG <38632001+NGCHUNKEUNG@users.noreply.github.com>
2018-05-23 23:49:15 +1000

    23-5-2018 update
    maybe the last update
    80% finish the work

--- Tap Game/GameMenu.swift
+++ Tap Game/GameMenu.swift
@@ -14,13 +14,13 @@ class GameMenu: SKScene{
     
     var startGame = SKLabelNode()
     var bestScore = SKLabelNode()
-   
+    var gameSettings = Settings.sharedInstance
     
     override func didMove(to view: SKView) {
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         startGame = self.childNode(withName: "startGame") as! SKLabelNode
         bestScore = self.childNode(withName: "bestScore") as! SKLabelNode
-       
+        bestScore.text = "Best : \(gameSettings.highScore)"
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -37,3 +37,5 @@ class GameMenu: SKScene{
     }
     
 }
+
+
